f9b2b97d2240ded0b4e50aa2a81e27e5856ac6f0
Syohei YOSHIDA <syohex@gmail.com>
2015-11-04 17:23:00 +0900

    Specify dash and s minimum version

--- wilt.el
+++ wilt.el
@@ -5,7 +5,7 @@
 ;; Author: Austin Bingham <austin@sixty-north.com>
 ;; Version: 0.1
 ;; URL: https://github.com/sixty-north/emacs-wilt
-;; Package-Requires: ((dash "X.Y.Z") (s "X.Y.Z"))
+;; Package-Requires: ((dash "2.12.0") (s "1.10.0"))
 ;;
 ;; This file is not part of GNU Emacs.
 ;;
